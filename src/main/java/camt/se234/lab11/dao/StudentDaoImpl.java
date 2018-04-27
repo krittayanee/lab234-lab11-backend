ffa7e339693708958f8fb72cbe780f93bb0eebc6
Unknown <thamaraja.jajar@hotmail.com>
2018-04-27 15:40:22 +0700

    travis#1

--- src/main/java/camt/se234/lab11/dao/StudentDaoImpl.java
+++ src/main/java/camt/se234/lab11/dao/StudentDaoImpl.java
@@ -10,7 +10,11 @@ public class StudentDaoImpl implements StudentDao {
     public StudentDaoImpl(){
         students = new ArrayList<>();
         students.add(new Student("123","A","temp",2.33));
-
+        students.add(new Student("5721105001","Prayuth","Kamsing",4.00));
+        students.add(new Student("5821105002","Somluck","Kamsing",2.50));
+        students.add(new Student("5821105003","Cherprang","Capt",3.50));
+        students.add(new Student("5921105008","Job","Sung",1.25));
+        students.add(new Student("6021155667","Hiroko","Basket",0.20));
     }
 
     @Override
--- src/test/java/camt/se234/lab11/service/StudentServiceImplTest.java
+++ src/test/java/camt/se234/lab11/service/StudentServiceImplTest.java
@@ -1,4 +1,27 @@
 package camt.se234.lab11.service;
 
+import camt.se234.lab11.dao.StudentDaoImpl;
+import camt.se234.lab11.entity.Student;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.number.IsCloseTo.closeTo;
+import static org.junit.Assert.assertThat;
+
 public class StudentServiceImplTest {
+    @Test
+    public void testFindById(){
+        StudentDaoImpl studentDao = new StudentDaoImpl();
+        StudentServiceImpl studentService = new StudentServiceImpl();
+        studentService.setStudentDao(studentDao);
+        assertThat(studentService.findStudentById("123"),is(new Student("123","A","temp",2.33)));
+    }
+
+    @Test
+    public void TestGetAverageGPA(){
+        StudentDaoImpl studentDao = new StudentDaoImpl();
+        StudentServiceImpl studentService = new StudentServiceImpl();
+        studentService.setStudentDao(studentDao);
+        assertThat(studentService.getAverageGpa(),is(closeTo(2.29,0.007)));
+    }
 }
